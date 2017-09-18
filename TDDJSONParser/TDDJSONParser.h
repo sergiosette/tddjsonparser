1001d7dcca909fa5dd10180c452132bfcfa03dd1
Sergio Sette <sergio.sette@grandparade.co.uk>
2017-09-18 19:43:55 +0200

    [0][GREEN] Project created

---
+++ TDDJSONParser/TDDJSONParser.h
@@ -0,0 +1,19 @@
+//
+//  TDDJSONParser.h
+//  TDDJSONParser
+//
+//  Created by Sergio Sette on 18/09/2017.
+//  Copyright © 2017 Sergio Sette. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TDDJSONParser.
+FOUNDATION_EXPORT double TDDJSONParserVersionNumber;
+
+//! Project version string for TDDJSONParser.
+FOUNDATION_EXPORT const unsigned char TDDJSONParserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TDDJSONParser/PublicHeader.h>
+
+
---
+++ TDDJSONParserTests/TDDJSONParserTests.swift
@@ -0,0 +1,14 @@
+//
+//  TDDJSONParserTests.swift
+//  TDDJSONParserTests
+//
+//  Created by Sergio Sette on 18/09/2017.
+//  Copyright © 2017 Sergio Sette. All rights reserved.
+//
+
+import XCTest
+@testable import TDDJSONParser
+
+class TDDJSONParserTests: XCTestCase {
+    
+}
