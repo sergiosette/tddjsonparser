01e0023ea8cb2f085dac0da6b7152303941abeb5
Sergio Sette <sergio.sette@grandparade.co.uk>
2017-10-26 21:11:26 +0200

    [12-22][GREEN] String parser

--- TDDJSONParserTests/TDDJSONParserTests.swift
+++ TDDJSONParserTests/TDDJSONParserTests.swift
@@ -16,24 +16,57 @@ class TDDJSONParserTests: XCTestCase {
     }
     
     func testEmpty() {
-        XCTAssertNil(parser.parse(""))
+        assertNil("")
     }
     
     func testTrue() {
-        XCTAssertTrue(parser.parse("true") as! Bool)
+        assert("true", parsesTo: true)
     }
     
     func testFalse() {
-        XCTAssertFalse(parser.parse("false") as! Bool)
+        assert("false", parsesTo: false)
     }
     
     func testNumber() {
-        XCTAssertEqual(parser.parse("0") as! Int, 0)
+        assert("0", parsesTo: Int(0))
+    }
+    
+    func testDouble() {
+        assert("0.0", parsesTo: Double(0.0))
+    }
+    
+    func testEmptyString() {
+        assert("\"\"", parsesTo: "")
+    }
+    
+    func testUnfinishedQuote() {
+        assertNil("\"")
+    }
+    
+    func testString() {
+        assert("\"aString\"", parsesTo: "aString")
+    }
+    
+    func assert<T: Equatable>(_ json: String, parsesTo target: T) {
+        guard let parsed = parser.parse(json) as? T else { XCTFail("Parsed item \(json) is not of type \(T.self)"); return }
+        XCTAssertEqual(parsed, target)
+    }
+    
+    func assertNil(_ json: String) {
+        XCTAssertNil(parser.parse(json))
     }
 }
 
 class JSONParser {
     func parse(_ json: String) -> Any? {
-        return Bool(json) ?? Int(json)
+        return Bool(json) ?? Int(json) ?? Double(json) ?? parseString(json)
+    }
+    
+    private func parseString(_ text: String) -> String? {
+        guard text.count > 1 else { return nil }
+        switch (text.first, text.last) {
+        case ("\""?, "\""?): return String(text.dropFirst().dropLast())
+        default: return nil
+        }
     }
 }
